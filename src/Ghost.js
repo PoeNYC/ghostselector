457a4bc39ce71c20ae54397aa38ece4bfe853e76
unknown <poehwang@yahoo.com>
2019-10-31 20:40:04 -0400

    Created a method for the click to represent the right ghost & added pictures

--- src/Ghost.js
+++ src/Ghost.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Ghost = (props) => {
+  const selectGhost = props.selectGhost;
+  const ghost = props.ghost;
   return(
     <div>
+      <img src = {ghost} alt ="" onClick = {() => selectGhost(ghost)} />
     </div>
   )
 };
